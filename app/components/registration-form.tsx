13fb5194278c506891bd702b325c652be3a2e0fe
Eamon Gordon <ekeokigordon@icloud.com>
2024-08-23 09:31:41 -0700

    feat: add login/signup capability to registration form

--- app/components/registration-form.tsx
+++ app/components/registration-form.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { createUser } from "@/actions"
-import { signIn } from "@/auth";
-import { useRouter } from 'next/navigation'
+import { SessionProvider, signIn } from "next-auth/react";
 
 export function RegistrationForm({ type }: { type: "login" | "signup" }) {
-    const router = useRouter();
     function handleSubmit(formData: FormData) {
         if (type === "signup") {
             createUser({
@@ -13,26 +11,33 @@ export function RegistrationForm({ type }: { type: "login" | "signup" }) {
                 password: formData.get("password") as string,
                 name: formData.get("name") as string ?? undefined
             }).then(() => {
-                router.push("/");
+                login(formData);
             }).catch((error) => {
                 alert("User already exists");
             });
         } else {
-            signIn("credentials", {
-                email: formData.get("email"),
-                password: formData.get("password"),
-                redirectTo: "/"
-            }).catch((error) => {
-                alert("Invalid email or password");
-            });
+            login(formData);
         }
     }
+    function login(formData: FormData) {
+        signIn("credentials", {
+            email: formData.get("email"),
+            password: formData.get("password"),
+            callbackUrl: "/"
+        }).catch((error) => {
+            alert("Invalid email or password");
+        });
+    }
     return (
-        <form action={handleSubmit}>
-            {type === "signup" && <input type="text" placeholder="Name" />}
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">{type === "signup" ? "Sign Up" : "Login"}</button>
-        </form>
+        <SessionProvider>
+            <div className="flex items-center justify-center h-screen w-screen">
+                <form action={handleSubmit} className="flex flex-col gap-2 items-center justify-center max-w-screen-sm">
+                    {type === "signup" && <input className="rounded-md p-2" type="text" placeholder="Name" />}
+                    <input className="rounded-md p-2" type="email" placeholder="Email" />
+                    <input className="rounded-md p-2" type="password" placeholder="Password" />
+                    <button className="border-2 p-2 rounded-md bg-white dark:bg-black" type="submit">{type === "signup" ? "Sign Up" : "Login"}</button>
+                </form>
+            </div>
+        </SessionProvider>
     )
 }
